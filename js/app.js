9d2002f3079aa9f4d0709126bade8fbfbe475de1
Natasha Clark <Natasharclark@gmail.com>
2017-03-09 08:29:06 -0500

    index set up

--- js/app.js
+++ js/app.js
@@ -1,5 +1,36 @@
 "use strict";
 
-(function(){
-  angular.module("wdinstagram", [])
-})();
+let wdiData = [
+  { photo_url: 'http://i.imgur.com/XdcsOZL.png', author: 'John', body: 'doing stuff' },
+  { photo_url: 'http://i.imgur.com/3uDxPBl.jpg', author: 'Sarah', body: 'doing more stuff' },
+  { photo_url: 'http://i.imgur.com/gzdT63J.jpg', author: 'Kelly', body: 'doing even more stuff' },
+  { photo_url: 'http://i.imgur.com/UdNZUdf.jpg', author: 'Susan', body: 'doing canadian stuff' },
+  { photo_url: 'http://i.imgur.com/t2QHpLL.jpg', author: 'Bob', body: 'doing weird stuff' }
+]
+
+  angular
+    .module("wdinstagram", ["ui.router"])
+    .controller("wdinstagramController", ["$stateParams", wdinstagramControllerFunction])
+    .config(["$stateProvider", RouterFunction])
+
+  function RouterFunction ($stateProvider){
+    $stateProvider
+      .state("wdiIndex", {
+        url: "/",
+        templateUrl: "wdi-index.html",
+        controller: "wdinstagramController",
+        controlllerAs: "vm"
+      })
+      .state("wdiShow", {
+        url: "/wdis/:id",
+        templateUrl: "wdi-show.html",
+        controller: "wdinstagramShowController",
+        controlllerAs: "vm"
+      })
+  }
+  function wdinstagramControllerFunction ($state, $stateParams) {
+    this.wdis= wdiData;
+    this.create = function (){
+      this.wdis.$add(this.newWdi).then( () => this.newWdi = {} )
+    }
+  }
